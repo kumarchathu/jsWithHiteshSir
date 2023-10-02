474433964503d477da17cf5f1daab32c01ae052c
kumarchathu <kumarchathu2530@gmail.com>
2023-10-02 10:43:16 +0530

    wih comment

--- app1/Basic1.js
+++ app1/Basic1.js
@@ -1,1 +1,14 @@
 console.log("hello javaScript");
+// run first javaScript program
+// step by step
+// -----------------
+// 1) go to the view --> terminasl (ctrl + shift + '~`')
+// ~` <-------------- near by 1 key
+// first we need to go to same path (need to reach parent folder)
+// if you not reached parent folder and run cmd then get Error
+// node:internal/modules/cjs/loader:1080
+//   throw err;
+//   ^
+// ex:-PS D:\web-technolgy\javaScript\program\jsWithHiteshSir\app1> 
+// 2) type `node Basic1.js` and press enter key
+//
